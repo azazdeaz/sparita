ec99973ee9932be1a68b23612fdb455b3de4eb4e
polgar andras <polgar.andras@ebolamedia.hu>
2014-06-16 10:09:24 +0200

    get threejs working

--- app/scripts/editor/Editor.js
+++ app/scripts/editor/Editor.js
@@ -4,46 +4,42 @@ var EdiBox = require('./EdiBox');
 
 function Editor(model) {
 
-	var x, y, z, ediBoxes = [], boxSize = [100, 100, 100],
-	camera, scene, renderer;
+  var x, y, z, ediBoxes = [], boxSize = [30, 30, 30],
+  camera, scene, renderer;
 
-	this._renderW = 400;
-	this._renderH = 300;
+  this._renderW = 400;
+  this._renderH = 300;
 
-	camera = new THREE.Camera( 45, this._renderW / this._renderH, .1, 1000 );
-	camera.position.z = 300;
+  camera = new THREE.PerspectiveCamera( 45, this._renderW / this._renderH, .1, 1000 );
+  camera.position.z = 300;
 
-	scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-	renderer = new THREE.WebGLRenderer({alpha: false});
-	renderer.setSize( this._renderW, this._renderH );
-	this.domElement = renderer.domElement;
+  renderer = new THREE.WebGLRenderer({alpha: false});
+  renderer.setSize( this._renderW, this._renderH );
+  this.domElement = renderer.domElement;
 
-	var pointLight = new THREE.PointLight(0xFFFFFF);
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
-	// add to the scene
-	scene.add(pointLight);
+  // var pointLight = new THREE.PointLight(0xFFFFFF);
+  // // set its position
+  // pointLight.position.x = 10;
+  // pointLight.position.y = 50;
+  // pointLight.position.z = 130;
+  // // add to the scene
+  // scene.add(pointLight);
 
-var ediBox = new EdiBox(model.boxDiv, boxSize);
-				scene.add(ediBox.mesh);
+  for (x = 0; x < model.div[0]; ++x) {
+    for (y = 0; y < model.div[1]; ++y) {
+      for (z = 0; z < model.div[2]; ++z) {
 
-	// for (x = 0; x < model.div[0]; ++x) {
-	// 	for (y = 0; y < model.div[1]; ++y) {
-	// 		for (z = 0; z < model.div[2]; ++z) {
-		
-	// 			var ediBox = new EdiBox(model.boxDiv, boxSize);
-	// 			scene.add(ediBox.mesh);
-	// 			// ediBox.mesh.position.y = 213
-	// 			ediBoxes.push(ediBox);
-	// 		}
-	// 	}
-	// }
+        var ediBox = new EdiBox(model.boxDiv, boxSize);
+        scene.add(ediBox.mesh);
+        // ediBox.mesh.position.y = 213
+        ediBoxes.push(ediBox);
+      }
+    }
+  }
 
-	renderer.render( scene, camera );
-	window.rrrrrr = function () {renderer.render( scene, camera );}
+  renderer.render( scene, camera );
 }
 
 var p = Editor.prototype;
