0629963cb3e49d25164da6240c35d8bc62fa9ca4
azazdeaz <azazdeaz@gmail.com>
2014-07-16 18:09:36 +0200

    parital

--- app/scripts/gamepage.js
+++ app/scripts/gamepage.js
@@ -1,107 +1,107 @@
 'use strict';
 
 var Editor = require('./editor/Editor'),
-    blueprint = require('./editor/blueprint'),
-    pager = require('./pager'),
-    modelSettings = require('./modals/modelSettings'),
-	template = require('../templates/gamepage.html');
+        blueprint = require('./editor/blueprint'),
+        pager = require('./pager'),
+        modelSettings = require('./modals/modelSettings'),
+    template = require('../templates/gamepage.html');
 
 var gamepage = {
 
-	$root: $(template),
-    $navbarAddon: $(
-        '<li><a class="btn-undo">undo</a></li>' +
-        '<li><a class="btn-redo">redo</a></li>' +
-        '<li><a class="btn-reset">reset</a></li>'
-    ),
+    $root: $(template),
+        $navbarAddon: $(
+                '<li><a class="btn-undo">undo</a></li>' +
+                '<li><a class="btn-redo">redo</a></li>' +
+                '<li><a class="btn-reset">reset</a></li>'
+        ),
 
-    setup: function (_opt) {
+        setup: function (_opt) {
 
-        var opt = gamepage._setupOpt = _.extend({
-            mode: 'editor',
-            initModel: {
-                div: {x: 2, y: 3, z: 4},
-                boxDiv: {x: 3, y: 3, z: 3}
-            }
-        }, _opt);
+                var opt = gamepage._setupOpt = _.extend({
+                        mode: 'editor',
+                        initModel: {
+                                div: {x: 2, y: 3, z: 4},
+                                boxDiv: {x: 3, y: 3, z: 3}
+                        }
+                }, _opt);
 
-        modelSettings.setName(opt.initModel.name || 'Unnamed model');
-        modelSettings.setBlueprintSides(opt.initModel.blueprintSides || ['front', 'right']);
+                modelSettings.setName(opt.initModel.name || 'Unnamed model');
+                modelSettings.setBlueprintSides(opt.initModel.blueprintSides || ['front', 'right']);
 
-        gamepage.editor = new Editor(opt.initModel);
-        gamepage.editor.setSize(window.innerWidth, window.innerHeight);
-        gamepage.$root.append(gamepage.editor.domElement);
+                gamepage.editor = new Editor(opt.initModel);
+                gamepage.editor.setSize(window.innerWidth, window.innerHeight);
+                gamepage.$root.append(gamepage.editor.domElement);
 
-        this.$root.find('.btn-settings .btn-save')[opt.mode === 'editor' ? 'show' : 'hide']();
-        this.$root.find('.btn-finish')[opt.mode === 'game' ? 'show' : 'hide']();
-
-        this.$navbarAddon.find('.btn-undo')(function () {gamepage.editor.undo();});
-        this.$navbarAddon.find('.btn-redo')(function () {gamepage.editor.redo();});
-        this.$navbarAddon.find('.btn-reset')(function () {gamepage.editor.reset();});
-    }
+                this.$root.find('.btn-settings .btn-save')[opt.mode === 'editor' ? 'show' : 'hide']();
+                this.$root.find('.btn-finish')[opt.mode === 'game' ? 'show' : 'hide']();
+        }
 };
 
 function init () {
 
-    gamepage.$root.find('.btn-test').click(testSolusion);
+        gamepage.$root.find('.btn-test').click(testSolusion);
 
-    gamepage.$root.find('.btn-settings').click(function () {
+        gamepage.$root.find('.btn-settings').click(function () {
 
-        pager.openModal('modelSettings');
-    });
+                pager.openModal('modelSettings');
+        });
 
-    modelSettings.on('change-blueprint-sides', function (sides) {
+        modelSettings.on('change-blueprint-sides', function (sides) {
 
-        gamepage.editor.getOriginModel().blueprintSides = sides;
+                gamepage.editor.getOriginModel().blueprintSides = sides;
 
-        refreshBlueprints();
-    });
+                refreshBlueprints();
+        });
+
+        this.$navbarAddon.find('.btn-undo')(function () {gamepage.editor.undo();});
+        this.$navbarAddon.find('.btn-redo')(function () {gamepage.editor.redo();});
+        this.$navbarAddon.find('.btn-reset')(function () {gamepage.editor.reset();});
 }
 
 function testSolusion() {
 
-    var model = gamepage.editor.getModel(gamepage._setupOpt);
+        var model = gamepage.editor.getModel(gamepage._setupOpt);
 
-    if (blueprint.testSolusion(model, gamepage.setupOpt.targetModel)) {
+        if (blueprint.testSolusion(model, gamepage.setupOpt.targetModel)) {
 
-        handleWin();
-    }
+                handleWin();
+        }
 }
 
 function saveModel() {
 
-    var model = gamepage.editor.getModel();
-    back.saveModel(model);
+        var model = gamepage.editor.getModel();
+        back.saveModel(model);
 }
 
 function handleWin() {
 
-    pager.openModal('win');
+        pager.openModal('win');
 }
 
 function refreshBlueprints() {
 
-    var model = gamepage.editor.getModel(gamepage._setupOpt), prints = [], fullH,
-        $blueprintCont = gamepage.$root.find('.blueprint-cont').empty();
+        var model = gamepage.editor.getModel(gamepage._setupOpt), prints = [], fullH,
+                $blueprintCont = gamepage.$root.find('.blueprint-cont').empty();
 
-    model.blueprintSides.forEach(function (sideName) {
+        model.blueprintSides.forEach(function (sideName) {
 
-        prints.push(blueprint.print({
-            model: model,
-            side: sideName,
-            name: sideName.chareAt(0).toUpperCase + sideName.substr(1)
-        }));
-    });
+                prints.push(blueprint.print({
+                        model: model,
+                        side: sideName,
+                        name: sideName.chareAt(0).toUpperCase + sideName.substr(1)
+                }));
+        });
 
-    fullH = prints.reduce(function (a, b) {return a + b.height;}, 0);
+        fullH = prints.reduce(function (a, b) {return a + b.height;}, 0);
 
-    prints.forEach(function (print) {
+        prints.forEach(function (print) {
 
-        print.style.right = '0';
-        print.style.height = ((print.height / fullH) * 100) + '%';
-        print.style.width = 'auto';
-        $blueprintCont.append(print);
-    });
+                print.style.right = '0';
+                print.style.height = ((print.height / fullH) * 100) + '%';
+                print.style.width = 'auto';
+                $blueprintCont.append(print);
+        });
 }
 
 
